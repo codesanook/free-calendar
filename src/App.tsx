f3e2f3db3daaa4ba6bbfad6176b81478380f32df
aaronamm <theeranitp@gmail.com>
2025-04-26 22:34:07 +0700

    Finish simple calendar

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './app.scss'
+import EventCalendar from './Calendar'
+
+function App() {
+  return (
+    <EventCalendar />
+  )
+}
+
+export default App
---
+++ src/Calendar.tsx
@@ -0,0 +1,91 @@
+import {
+  addDays,
+  addMonths,
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  getMonth,
+  isToday,
+  startOfMonth,
+  startOfWeek,
+  startOfYear,
+  subMonths,
+} from 'date-fns'
+
+import { useState, MouseEvent } from 'react';
+import { match, P } from 'ts-pattern';
+
+export default function Calendar() {
+
+  const [initialDate, setInitialDate] = useState(new Date());
+  const changeInitialDate = (monthIndex: number) => (_: MouseEvent<HTMLElement>) => {
+    const diff = monthIndex - getMonth(initialDate);
+
+    const result = match(diff)
+      .with(0, () => initialDate)
+      .with(P.number.lt(0), () => subMonths(initialDate, Math.abs(diff)))
+      .with(P.number.gt(0), () => addMonths(initialDate, diff))
+      .otherwise(() => { throw new Error(); });
+
+    setInitialDate(result);
+  }
+
+  const firstDayOfWeek = startOfWeek(initialDate)
+  const dayNames = Array.from({ length: 7 }).map(
+    (_, i) => format(addDays(firstDayOfWeek, i), 'E').toUpperCase()
+  );
+
+  const firstDayOfMonth = startOfMonth(initialDate);
+  const lastDayOfMonth = endOfMonth(initialDate);
+
+  const daysInMonth = eachDayOfInterval(
+    {
+      start: firstDayOfMonth,
+      end: lastDayOfMonth
+    }
+  );
+
+  // index starts from 0 - 6
+  const startDayIndex = getDay(firstDayOfMonth);
+  const endDayIndex = getDay(lastDayOfMonth);
+
+  const firstMonth = startOfYear(initialDate);
+  const monthNames = Array.from({ length: 12 }).map(
+    (_, i) => format(addMonths(firstMonth, i), 'MMM').toUpperCase()
+  );
+
+  return (
+    <div className='container'>
+      <h1 className='calendar-title'>Free calendar - ปฎิทินแจกฟรี</h1>
+      <div className='day-grid'>
+        {dayNames.map((d, index) =>
+          <div key={index} className='head'>{d}</div>
+        )}
+        {
+          Array.from({ length: startDayIndex }).map((_, index) =>
+            <div key={index}></div>
+          )
+        }
+
+        {daysInMonth.map((day, index) =>
+          <div key={index} className={(isToday(day) ? '_highlight' : '')}>{format(day, 'd')}</div>
+        )}
+
+        {
+          Array.from({ length: 6 - endDayIndex }).map((_, index) =>
+            <div key={index}></div>
+          )
+        }
+      </div>
+
+      <div className='months'>
+        {monthNames.map((m, monthIndex) =>
+          <div key={monthIndex} className={(getMonth(initialDate) == monthIndex ? '_highlight' : '')} onClick={changeInitialDate(monthIndex)}>
+            {m}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
