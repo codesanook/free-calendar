a2753b0cca4566b03810bded679b894cad427c83
aaronamm <theeranitp@gmail.com>
2025-05-12 23:33:54 +0700

    Move to React 18 + external CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
+
+// https://github.com/Eptagone/Vite.AspNetCore/blob/main/examples/libraries/ViteNET.React/vite.config.ts#L69
+// https://rollupjs.org/configuration-options/#input
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // https://stackoverflow.com/a/74192065
+    cssCodeSplit: false,
+    rollupOptions: {
+      plugins: [],
+      external: ['react', 'react-dom'],
+      output: {
+        format: 'umd',
+        globals: {
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+        },
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      }
+    }
+  }
+});
